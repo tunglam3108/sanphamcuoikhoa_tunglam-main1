e9ef99745f5df71b9eb3af7f321a0850ec934b4c
tunglam3108 <dotunglam3108@gmail.com>
2024-04-30 22:47:41 +0700

    data

--- js/quanaodata.js
+++ js/quanaodata.js
@@ -22,16 +22,39 @@ const products = [
         "name": "Áo khoác bơi",
         "image": "../img/quần áo/áo khoác bơi.png",
         "price": "300 $"
+    },
+    {
+        "id": 5,
+        "name": "Váy bơi",
+        "image": "link_hinh_anh_5.jpg",
+        "price": "300 $"
+    },
+    {
+        "id": 6,
+        "name": "Quần shorts",
+        "image": "../img/quần áo/quần shorts.png",
+        "price": "50 $"
+    },
+    {
+        "id": 7,
+        "name": "Áo thun",
+        "image": "../img/quần áo/áo thun.png",
+        "price": "200 $"
+    },
+    {
+        "id": 8,
+        "name": "Váy maxi",
+        "image": "../img/quần áo/váy maxi.png",
+        "price": "500 $"
     }
-
-]
+];
 
 let productLis = document.querySelector(".product-list")
 let sumOfProduct = ``;
 
 let sum = 0
 for (let key of products) {
-  if (sum < 4) {
+  if (sum < 8) {
     sumOfProduct += `
     <div class="product">
                 <div class="images"> <img src="${key.image}" alt=""></div>
--- js/vatdungdata.js
+++ js/vatdungdata.js
@@ -1,36 +1,60 @@
 const products = [
     {
         "id": 1,
+        "name": "Dù che nắng",
+        "image": "../img/vật dụng/dù che nắng.png",
+        "price": "100 $" 
+    },
+    {
+        "id": 2,
         "name": "Balo chống nước",
         "image": "../img/vật dụng/ba lô chống nước.png",
         "price": "200 $" 
     },
     {
-        "id": 2,
+        "id": 3,
         "name": "Kem chống nắng",
         "image": "../img/vật dụng/kem chống nắng.png",
         "price": "50 $" 
     },
     {
-        "id": 3,
+        "id": 4,
+        "name": "Máy ảnh chống nước",
+        "image": "../img/vật dụng/máy ảnh chống nước.png",
+        "price": "600 $" 
+    },
+    {
+        "id": 5,
+        "name": "Bình nước giữ nhiệt",
+        "image": "../img/vật dụng/bình giữ nhiệt.png",
+        "price": "30 $" 
+    },
+    {
+        "id": 6,
         "name": "Phao bơi",
         "image": "../img/vật dụng/phao bơi.png",
         "price": "100 $" 
     },
     {
-        "id": 4,
+        "id": 7,
         "name": "Kính râm",
         "image": "../img/vật dụng/kính râm.png",
         "price": "100 $" 
+    },
+    {
+        "id": 8,
+        "name": "Vớ bơi",
+        "image": "../img/vật dụng/vớ bơi.png",
+        "price": "50 $" 
     }
-]
+];
 
 let productLis = document.querySelector(".product-list")
 let sumOfProduct = ``;
 
 let sum = 0
 for (let key of products) {
-  if (sum < 4) {
+  if (sum < 8) {
     sumOfProduct += `
     <div class="product" id="${key.id}">
                 <div class="images"> <img src="${key.image}" alt=""></div>
